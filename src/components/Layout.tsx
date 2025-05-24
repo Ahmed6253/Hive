f8d0a62bce798099fc61fcdbc445b97f1cba2da9
Ahmed Mohamed <abdelmonaem535@gmail.com>
2025-05-25 00:07:46 +0300

    small change for a file name

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "./sidebar";
-import Loader from "./Loader";
+import Loader from "./loader";
 import { useEffect, useState } from "react";
 const Layout = () => {
   const [isLoading, setIsLoading] = useState(true);
