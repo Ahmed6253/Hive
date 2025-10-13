a3d96395e5244acb6a01708c33b840b34102a88f
Ahmed Mohamed <abdelmonaem535@gmail.com>
2025-10-13 22:48:37 +0300

    feat: add animations to modal component for smoother transitions and update styles

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,5 @@
 import { X } from "lucide-react";
+import { useState } from "react";
 
 export default function Modal({
   children,
@@ -11,21 +12,35 @@ export default function Modal({
   show: boolean;
   toggleShow: () => void;
 }) {
+  const [isClose, setIsClose] = useState(false);
+  const toggle = () => {
+    if (show) {
+      setIsClose(true);
+
+      const timer = setTimeout(() => {
+        toggleShow();
+        setIsClose(false);
+      }, 200);
+      return () => clearTimeout(timer);
+    }
+  };
   if (!show) {
     return null;
   }
   return (
     <div
-      onClick={toggleShow}
+      onClick={toggle}
       className="fixed top-0 left-0 w-full h-full bg-bg/80 backdrop-blur-[3px] flex justify-center items-center"
     >
       <div
         onClick={(e) => e.stopPropagation()}
-        className="bg-bg border border-text/40 h-[50vh] min-w-sm p-4 rounded-lg space-y-2"
+        className={`bg-bg border border-text/20 from-bottom h-[50vh] min-w-sm p-4 rounded-lg space-y-2 ${
+          isClose && "back-bottom"
+        }`}
       >
         <div className="flex justify-between">
           <h2 className="text-lg font-semibold">{title}</h2>
-          <button onClick={toggleShow} className="cursor-pointer">
+          <button onClick={toggle} className="cursor-pointer">
             <X className="w-4 h-4" />
           </button>
         </div>
